ac61bdd43b130817f30d2f637325f42a6d0867d3
Tyler Akin <tyler@appriss.com>
2021-11-02 23:00:28 -0400

    Reworks scss structure and deletes default themes

--- wp-content/themes/legacyfarm/template-components/layouts/partials/homepage-slider/latest-sermon.php
+++ wp-content/themes/legacyfarm/template-components/layouts/partials/homepage-slider/latest-sermon.php
@@ -1,5 +1,37 @@
-<div class="layout slider-partial latest-sermon-slider-partial">
-    <?php
-     include(get_template_directory() 
-     . '/template-components/layouts/partials/latest-sermon.php'); ?>
+<div class="slider-partial latest-sermon-slider-partial">
+    <?php include get_template_directory() . '/template-queries/latest-sermon.php'; ?>
+
+    <?php if ( $the_latest_sermon_query->have_posts() ) : while ( $the_latest_sermon_query->have_posts() ) : $the_latest_sermon_query->the_post(); ?>
+    <div class="layout latest-sermon slider-partial latest-sermon-slider-partial sermon">
+        <?php $featured_image = get_the_post_thumbnail_url(); ?>
+        <img class="latest-sermon__image" src="<?php echo $featured_image; ?>"/>
+
+        <div class="contain">
+            <h2 class="latest-sermon__heading">Latest Sermon</h2>
+           
+            <div class="latest-sermon__title">
+                <h3><?php the_title() ?></h3>
+            </div>
+
+            <div class="latest-sermon__content">
+                <?php the_content() ?>
+            </div>
+
+            <?php
+                $term_obj_list = get_the_terms( $the_latest_sermon_query->ID, 'series' ); ?>
+                <div class="blog-categories">
+                    <?php
+                    foreach ( $term_obj_list as $term ) {
+                        $term_link = get_term_link( $term ); ?>
+                        Current Series
+                        <a class="button" href="<?php echo $term_link ?>"><?php echo $term->name; ?></a>
+                    <?php } ?>
+                </div>
+
+            <a class="button view-more-sermons" href="/audubon-church/sermons">View More Sermons</a>
+        </div>
+    </div>
+
+    <?php endwhile; else: ?> <p>Sorry, there are no posts to display</p> <?php endif; ?>
+    <?php wp_reset_query(); ?>
 </div>
--- wp-content/themes/legacyfarm/template-components/layouts/partials/latest-sermon.php
+++ wp-content/themes/legacyfarm/template-components/layouts/partials/latest-sermon.php
@@ -1,24 +1,25 @@
-<?php
-    $args = array(
-    'orderby' => 'title',
-    'post_type' => 'sermons',
-    'posts_per_page' => 1,
-    'orderby' => 'date',
-    'order' => 'DESC', // Gets the most recent sermon post
-    );
-    $the_query = new WP_Query( $args );
-    ?>
+<?php include get_template_directory() . '/template-queries/latest-sermon.php'; ?>
 
-    <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+<?php if ( $the_latest_sermon_query->have_posts() ) : while ( $the_latest_sermon_query->have_posts() ) : $the_latest_sermon_query->the_post(); ?>
 
-    <div class="layout latest-sermon sermon">
-        <?php $featured_image = get_the_post_thumbnail_url(); ?>
-        <img class="latest-sermon__image" src="<?php echo $featured_image; ?>"/>
+<div class="layout latest-sermon sermon">
+    <?php $featured_image = get_the_post_thumbnail_url(); ?>
+    <img class="latest-sermon__image" src="<?php echo $featured_image; ?>"/>
 
-        <div class="contain">
-            <h2>Latest Sermon</h2>
+    <div class="contain">
+        <h2 class="latest-sermon__heading">Latest Sermon</h2>
+       
+        <div class="latest-sermon__title">
+            <h3><?php the_title() ?></h3>
+        </div>
+
+        <div class="latest-sermon__content">
+            <?php the_content() ?>
+        </div>
+
+        <div class="bottom-things">
             <?php
-                $term_obj_list = get_the_terms( $the_query->ID, 'series' ); ?>
+                $term_obj_list = get_the_terms( $the_latest_sermon_query->ID, 'series' ); ?>
                 <div class="blog-categories">
                     <?php
                     foreach ( $term_obj_list as $term ) {
@@ -26,17 +27,10 @@
                         <a href="<?php echo $term_link ?>"><?php echo $term->name; ?></a>
                     <?php } ?>
                 </div>
-            <div class="latest-sermon__title">
-                <h3><?php the_title() ?></h3>
-            </div>
-
-            <div class="latest-sermon__content">
-                <?php the_content() ?>
-            </div>
-
             <a class="button" href="/audubon-church/sermons">View More Sermons</a>
         </div>
     </div>
+</div>
 
-    <?php endwhile; else: ?> <p>Sorry, there are no posts to display</p> <?php endif; ?>
-    <?php wp_reset_query(); ?>
+<?php endwhile; else: ?> <p>Sorry, there are no posts to display</p> <?php endif; ?>
+<?php wp_reset_query(); ?>
